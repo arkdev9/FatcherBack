65d43caea9f9bfcc93428a1ce94d7e19c0c594f5
Abhishek Kasireddy <akasireddy99@gmail.com>
2020-05-30 15:40:20 +0530

    feat: user strict
    Users won't be able to submit on the same path

--- models/Report.js
+++ models/Report.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ReportSchema = mongoose.Schema({
 	// _id is the page url
 	_id: { type: String },
-	reports: { type: Number, default: 0 },
+	reports: { type: [String], default: [] },
 });
 
 module.exports = mongoose.model("report", ReportSchema, "reports");
